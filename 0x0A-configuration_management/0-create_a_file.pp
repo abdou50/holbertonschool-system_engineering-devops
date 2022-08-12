e1c1aaeb4df25db5a7b56a487e794859d95f1008
abdou50 <gadourraddadi@gmail.com>
2022-08-12 23:17:57 +0300

    l

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+#pip
+
 file  {'school':
     path    => '/tmp/school',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    content => 'I love Puppet'
+    content => 'I love Puppet',
 }
