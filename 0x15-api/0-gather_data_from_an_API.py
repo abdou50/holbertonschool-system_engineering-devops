a4d974303d8236436ef675f0a041d3e2a20494d6
abdou50 <gadourraddadi@gmail.com>
2022-09-13 01:43:53 +0300

    l

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """api"""
+
 import requests
 from sys import argv
 
 if __name__ == "__main__":
     """api"""
-
-
-req1 = requests.get('https://jsonplaceholder.typicode.com//todos')
-req2 = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                    .format(argv[1]))
-jj = req1.json()
-jk = req2.json()
-list = []
-todo = 0
-complited = 0
-for i in jj:
-    if (((i["userId"]) == int(argv[1]))):
-        todo += 1
-        if (i["completed"]):
-            y += 1
-            list = list + [(i["title"])]
-print("Employee {} is done with tasks({}/{}):".format(
-    jk["name"], complited, todo))
-for i in list:
-    print("\t", i)
+    list = []
+    req1 = requests.get('https://jsonplaceholder.typicode.com/todos')
+    req2 = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(argv[1]))
+    req1 = req1.json()
+    req2 = req2.json()
+    al = 0
+    done = 0
+    for i in req1:
+        if (((i["userId"]) == int(argv[1]))):
+            al += 1
+            if (i["completed"]):
+                done += 1
+                list = list + [(i["title"])]
+    print("Employee {} is done with tasks({}/{}):".format(
+        req2["name"], done, al))
+    for i in list:
+        print("\t", i)
