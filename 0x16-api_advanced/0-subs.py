b3eec6aab0a363a269d5a0033d016d2f60e5f1b4
abdou50 <gadourraddadi@gmail.com>
2022-09-28 00:05:49 +0300

    l

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,8 @@ import requests
 def number_of_subscribers(r):
 
     req = requests.get("https://www.reddit.com/r/{}/about.json".format(r),
-                       headers={"User-Agent": "My-User-Agent"}, allow_redirects=False)
+                       headers={"User-Agent": "My-User-Agent"},
+                       allow_redirects=False)
     if req.status_code >= 300:
         return 0
     mydic = req.json()
