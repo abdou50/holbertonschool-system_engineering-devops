47c6b4341cdf6dee1134205a71db533858368fa4
abdou50 <gadourraddadi@gmail.com>
2022-09-28 01:00:12 +0300

    l

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,11 +5,13 @@ import requests
 
 def top_ten(r):
     """function"""
-    req = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10".format(r),
+    req = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                       .format(r),
                        headers={"User-Agent": "My-User-Agent"},
                        allow_redirects=False)
     if req.status_code >= 300:
         print('None')
+
     else:
         mydic = req.json()
         for child in mydic.get("data").get("children"):
