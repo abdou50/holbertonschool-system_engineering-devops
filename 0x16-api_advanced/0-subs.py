e28a8df22bb81cea7ea6d7d6e64b1c3e4240d115
abdou50 <gadourraddadi@gmail.com>
2022-09-28 00:06:52 +0300

    l

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,7 @@ import requests
 
 
 def number_of_subscribers(r):
-
+    """function"""
     req = requests.get("https://www.reddit.com/r/{}/about.json".format(r),
                        headers={"User-Agent": "My-User-Agent"},
                        allow_redirects=False)
