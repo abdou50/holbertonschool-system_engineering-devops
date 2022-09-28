b58846e33953b961084c4b5329615251a0ddccb2
abdou50 <gadourraddadi@gmail.com>
2022-09-28 19:59:59 +0300

    l

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-#apache
-
-exec {'delp';
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	provider => 'shell',
+# Apache is returning error
+exec { 'delp':
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  provider => 'shell',
 }
