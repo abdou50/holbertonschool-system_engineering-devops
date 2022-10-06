4e8648f868121b00e3ea7c3653d493f5a0259ade
abdou50 <gadourraddadi@gmail.com>
2022-10-06 23:33:00 +0300

    l

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 #debug4
 
 exec {'modify max open files limit setting':
-  command => 'sed -i "s/15/8000/" /etc/default/nginx && sudo service nginx start',
+  command => 'sed -i "s/15/8000/" /etc/default/nginx && sudo service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
